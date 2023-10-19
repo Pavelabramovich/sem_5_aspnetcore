6a6633b481772e81f700dfa3b0b9a2887076a1cd
Pavelabramovich <apn20040510@gmail.com>
2023-10-19 15:39:41 +0300

    add identity server project

---
+++ BookShop.IdentityServer/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookShop.IdentityServer.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
