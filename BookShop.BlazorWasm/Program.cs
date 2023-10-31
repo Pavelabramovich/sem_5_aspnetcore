babcc3870fc4ef2dd1989c54333db62d55bdc135
Pavelabramovich <apn20040510@gmail.com>
2023-11-01 00:47:16 +0300

    fix service to scoped

--- BookShop.BlazorWasm/Program.cs
+++ BookShop.BlazorWasm/Program.cs
@@ -9,7 +9,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddSingleton<IDataService, DataService>();
+builder.Services.AddScoped<IDataService, DataService>();
 
 var apiUri = builder.Configuration["UriData:ApiUri"];
 
